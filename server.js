ce79924aed2117fa24919b521251b1f10427329d
First-repo <raoadusahmad@gmail.com>
2025-08-01 10:30:12 -0700

    Hope is Dope

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-cloudinary.config({
+console.log("Cloudinary config:", {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 
 app.post("/delete-image", async (req, res) => {
