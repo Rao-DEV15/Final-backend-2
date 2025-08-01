ec7a1c22d98e5b9732779562b9c496bab02bdb6a
First-repo <raoadusahmad@gmail.com>
2025-08-01 10:42:50 -0700

    Good

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
+
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-console.log("Cloudinary config:", {
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 
 app.post("/delete-image", async (req, res) => {
